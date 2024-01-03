19274801518f754a07b9b00d8c81aad1c9875246
LanceDark <dominik.maeder.97@web.de>
2024-01-03 14:14:56 +0100

    added players

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,33 +1,43 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { Game } from '../../models/game';
+import { PlayerComponent } from '../player/player.component';
 
 @Component({
   selector: 'app-game',
   standalone: true,
-  imports: [CommonModule],
+  imports: [CommonModule, PlayerComponent],
   templateUrl: './game.component.html',
-  styleUrl: './game.component.scss'
+  styleUrl: './game.component.scss',
 })
-
 export class GameComponent {
   pickCardAnimation = false;
+  currentCard!: string;
   game!: Game;
 
-  constructor(){
+  constructor() {}
 
-  }
-
-  ngOnInit(): void{ 
+  ngOnInit(): void {
     this.newGame();
-    console.log(this.game)
+    console.log(this.game);
   }
 
-  newGame(){
+  newGame() {
     this.game = new Game();
   }
 
-  takeCard(){
-    this.pickCardAnimation = true;
+  takeCard() {
+    if (!this.pickCardAnimation) {
+      let card = this.game.stack.pop();
+      if (card) {
+        this.currentCard = card;
+      }
+
+      this.pickCardAnimation = true;
+      setTimeout(() => {
+        this.game.playedCards.push(this.currentCard);
+        this.pickCardAnimation = false;
+      }, 1000);
+    }
   }
 }

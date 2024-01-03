f39bcee607560d3419a05c3eb48598557a3939df
LanceDark <dominik.maeder.97@web.de>
2024-01-03 13:10:00 +0100

    added new Game

--- src/app/game/game.component.ts
+++ src/app/game/game.component.ts
@@ -1,5 +1,6 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
+import { Game } from '../../models/game';
 
 @Component({
   selector: 'app-game',
@@ -8,9 +9,23 @@ import { CommonModule } from '@angular/common';
   templateUrl: './game.component.html',
   styleUrl: './game.component.scss'
 })
+
 export class GameComponent {
   pickCardAnimation = false;
+  game!: Game;
 
+  constructor(){
+
+  }
+
+  ngOnInit(): void{ 
+    this.newGame();
+    console.log(this.game)
+  }
+
+  newGame(){
+    this.game = new Game();
+  }
 
   takeCard(){
     this.pickCardAnimation = true;
